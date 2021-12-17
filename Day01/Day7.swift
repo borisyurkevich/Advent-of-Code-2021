c14bcb5cb0c4ffebefe14bc1da2a3b3581ecac91
Boris Yurkevich <boris@cocoaswitch.com>
2021-12-17 11:48:57 +0000

    Day 7 part 1

---
+++ Day01/Day7.swift
@@ -0,0 +1,66 @@
+//
+//  Day7.swift
+//  Day01
+//
+//  Created by Boris Yurkevich on 17/12/2021.
+//
+
+import Foundation
+
+enum Day7 {
+
+    static func run() {
+        let input = readFile("Day7.txt")
+        let positions = input.split(separator: ",")
+            .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+            .compactMap (Int.init)
+
+        let aligner = HorizontalAligner(positions: positions)
+        let (pos, cost) = aligner.minCostToAlign()
+
+        print("al at \(pos)")
+        print("min cost is: \(cost)")
+    }
+}
+
+struct HorizontalAligner {
+
+    let positions: [Int]
+
+    init(positions: [Int]) {
+        self.positions = positions
+    }
+
+    func costToMove(to target: Int) -> Int {
+        let costs = positions.map { position in
+            abs(target - position)
+        }
+
+        for (p, cost) in zip(positions, costs) {
+            print("[\(p)] -> \(target) cost \(cost)")
+        }
+        return costs.reduce(0, +)
+    }
+
+    func minCostToAlign() -> (pos: Int, cost: Int) {
+        guard let minPos = positions.min(),
+              let maxPos = positions.max() else {
+                  return (0, 0)
+              }
+
+        var pos = 0
+        var minCost =  Int.max
+        (minPos...maxPos).forEach { p in
+            let cost = costToMove(to: p)
+            if cost < minCost {
+                minCost = cost
+                pos = p
+            }
+        }
+        return (pos, minCost)
+    }
+}
+
+struct CrabSubmarine {
+
+}
--- Day01/main.swift
+++ Day01/main.swift
@@ -8,7 +8,7 @@
 //
 
 import Foundation
-import Algorithms
+//import Algorithms
 import CloudKit
 
 func readFile(_ name: String) -> String {
@@ -229,29 +229,30 @@ struct DepthScanner {
     }
 
     func increaseCount(windowSize: Int = 1) -> Int {
-
-        assert(windowSize > 0)
-        var windowSums: [Int] = []
-        for index in depths.indices {
-            if index < windowSize - 1 {
-                continue
-            }
-            var sum = 0
-            for i in 0..<windowSize {
-                sum += depths[index - i]
-            }
-            windowSums.append(sum)
-
-        }
-
-        for pair in windowSums.adjacentPairs() {
-            let diff = pair.1 - pair.0
-            print("[\(pair.0) -> \(pair.1)] \(diff)")
-        }
-        return windowSums.adjacentPairs()
-            .map { $1 - $0}
-            .filter { $0 > 0 }
-            .count
+        0
+//
+//        assert(windowSize > 0)
+//        var windowSums: [Int] = []
+//        for index in depths.indices {
+//            if index < windowSize - 1 {
+//                continue
+//            }
+//            var sum = 0
+//            for i in 0..<windowSize {
+//                sum += depths[index - i]
+//            }
+//            windowSums.append(sum)
+//
+//        }
+//
+//        for pair in windowSums.adjacentPairs() {
+//            let diff = pair.1 - pair.0
+//            print("[\(pair.0) -> \(pair.1)] \(diff)")
+//        }
+//        return windowSums.adjacentPairs()
+//            .map { $1 - $0}
+//            .filter { $0 > 0 }
+//            .count
     }
 
     static func parse(_ input: String) -> Self {
@@ -429,7 +430,8 @@ enum Day4 {
 }
 
 
-Day6.run()
+//Day6.run()
+Day7.run()
 
 
 extension Int {
