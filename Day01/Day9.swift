6fa385bde6178d71e1d96689ee1cca9624177c20
Boris Yurkevich <boris@cocoaswitch.com>
2021-12-23 16:28:53 +0300

    Day 9 Part 2

--- Day01/Day9.swift
+++ Day01/Day9.swift
@@ -18,22 +18,90 @@ enum Day9 {
         }
         let map = HeightMap(values: values)
         let risks = map.lowPointRisks()
-        print(risks)
+        print("risks: \(risks)")
         let sum = risks.reduce(0, +)
         print("Sum of low points: \(sum)")
+
+        let basins = map.findBaisins()
+        print("Basins: \(basins)")
+
+        guard basins.count >= 3 else { return }
+
+        let bb = basins.sorted().reversed()[0...2]
+        print("biggest basins 3: \(bb)")
+        print("basin product: \(bb.reduce(1, *))")
     }
 }
 
 struct HeightMap {
-    let values: [[Int]]
-
-    struct Coordinate: Hashable {
+    struct Coordinate: Hashable, CustomStringConvertible {
         let row: Int
         let col: Int
+
+        var description: String {
+            "[\(col), \(row)]"
+        }
     }
 
-    func lowPoints() -> [Int] {
-        var lowPoints: [Int] = []
+    let values: [[Int]]
+
+    var mapHeight: Int {
+        values.count
+    }
+
+    var mapWidth: Int {
+        values.first?.count ?? 0
+    }
+
+    func isValidCoordinate(_ c: HeightMap.Coordinate) -> Bool {
+        return c.col >= 0 && c.col < mapWidth && c.row > 0 && c.row < mapHeight
+    }
+
+    func look(from: HeightMap.Coordinate, offset: HeightMap.Coordinate) -> (coord: HeightMap.Coordinate, height: Int)? {
+        let newCoordinate = from + offset
+        guard isValidCoordinate(newCoordinate) else { return nil }
+        return (newCoordinate, height(at: newCoordinate))
+    }
+
+    func findBaisins() -> [Int] {
+        var basins: [Int] = []
+        var knownBaisinCoords: Set<HeightMap.Coordinate> = []
+
+        for lowPoint in lowPoints() {
+            var currentBaisinSize = 1
+            let lowPointHeight = height(at: lowPoint)
+
+            let basinCoordinates = neighborsSlopingUpward(from: lowPoint) + [lowPoint]
+
+            knownBaisinCoords.formUnion(basinCoordinates)
+
+            basins.append(basinCoordinates.count)
+        }
+
+        return basins
+    }
+
+    func neighborsSlopingUpward(from: HeightMap.Coordinate) -> [HeightMap.Coordinate] {
+        let thisHeight = height(at: from)
+        var results: Set<Coordinate> = []
+        let filteredNeigbors = neighbors(for: from)
+            .filter { n in
+                let h = height(at: n)
+                return h != 9 && h > thisHeight
+            }
+
+        for n in filteredNeigbors {
+            if !results.contains(n) {
+                results.insert(n)
+                neighborsSlopingUpward(from: n).forEach { results.insert($0) }
+            }
+        }
+
+        return Array(results)
+    }
+
+    func lowPoints() -> [Coordinate] {
+        var lowPoints: [Coordinate] = []
         var knownHighPointIndices: Set<HeightMap.Coordinate> = []
 
         for row in 0..<values.count {
@@ -48,7 +116,7 @@ struct HeightMap {
                 }
                 let thisHeight = height(at: coord)
                 if neigborHeights.allSatisfy({ $0.height > thisHeight }) {
-                    lowPoints.append(thisHeight)
+                    lowPoints.append(coord)
                     knownHighPointIndices.formUnion(neighbors)
                 }
 
@@ -58,7 +126,9 @@ struct HeightMap {
     }
 
     func lowPointRisks() -> [Int] {
-        lowPoints().map { $0 + 1 }
+        lowPoints().map { c in
+            height(at: c)
+        }.map { $0 + 1 }
     }
 
     func height(at coordinate: Coordinate) -> Int {
@@ -82,5 +152,12 @@ struct HeightMap {
             neighbors.insert(.init(row: c.row, col: c.col + 1))
         }
         return neighbors
+    }
+}
+
+extension HeightMap.Coordinate {
+
+    static func + (lhs: Self, rhs: Self) -> Self {
+        .init(row: lhs.row + rhs.row, col: lhs.col + rhs.col)
     }
 }
