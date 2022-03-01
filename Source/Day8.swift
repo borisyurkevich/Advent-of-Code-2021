c96f56d2feb77459e47233d86d85c26c8b9752e8
Boris Yurkevich <boris@cocoaswitch.com>
2022-03-01 18:32:19 +0000

    Day 8 part 2 is done

--- Source/Day8.swift
+++ Source/Day8.swift
@@ -1,6 +1,6 @@
 //
 //  Day8.swift
-//  Day01
+//  https://www.geeksforgeeks.org/seven-segment-displays/
 //
 //  Created by Boris Yurkevich on 23/12/2021.
 //
@@ -10,46 +10,113 @@ import Foundation
 enum Day8 {
 
     static func run() {
-        let file = readFile("Day8.txt")
-        let displays = file.lines.map(Display.init)
+        let input = readFile("Day8.txt")
 
-        print("displays: \(displays)")
+        let displays = input.lines.map(Display.init)
+        let outputs = displays.map(DisplaySolver.init).map { solver in
+            solver.solve()
+        }
+        print(outputs)
 
-        let count = displays
-            .flatMap(\.outputDigits)
-            .filter { d in
-                is1(d) || is4(d) || is7(d) || is8(d)
-            }
-            .count
+        let sum = outputs.reduce(0, +)
+        print("sun is \(sum)")
+//        let sum = outputs.reduce(0, +)
+//        print("sun is \(sum)")
+    }
+}
 
-        print(count)
+class DisplaySolver {
+    let display: Display
+
+    var possibleMatches: [Character: [Character]] = [:]
+
+    init(display: Display) {
+        self.display = display
     }
 
-    static func is1(_ segments: String) -> Bool {
+    func solve() -> Int {
+        let allSegments = "abcdefg"
+        for char in allSegments {
+            possibleMatches[char] = allSegments.map { $0 }
+        }
+
+        let one = display.patterns.first(where: { $0.count == 2 })!
+        let seven = display.patterns.first(where: { $0.count == 3 })!
+        let four = display.patterns.first(where: { $0.count == 4 })!
+        let six = display.patterns.first(where: { $0.count == 6 && !$0.isStrictSuperset(of: one) })!
+        let nine = display.patterns.first(where: { $0.count == 6 && $0.isStrictSuperset(of: four) })!
+        let zero = display.patterns.first(where: { $0.count == 6 && $0 != nine && $0 != six })!
+        let five = display.patterns.first(where: { $0.count == 5 && $0.isStrictSubset(of: six)})!
+        let eight = display.patterns.first(where: { $0.count == 7 })!
+        let three = display.patterns.first(where: { $0.count == 5 && $0.isStrictSubset(of: nine) && $0 != five})
+        let two = display.patterns.first(where: { $0.count == 5 && $0 != three && $0 != five })!
+
+        let lookup = [
+            zero: 0,
+            one: 1,
+            two: 2,
+            three: 3,
+            four: 4,
+            five: 5,
+            six: 6,
+            seven: 7,
+            eight: 8,
+            nine: 9
+        ]
+
+        let displayValue = display.outputDigits.enumerated().reduce(0, { sum, pair in
+            let digit = lookup[pair.element]!
+            let value = Int(pow(10.0, Double(display.outputDigits.count - 1 - pair.offset))) * digit
+            return sum + value
+        })
+
+        return displayValue
+    }
+
+    private func updateMatches(for pattern: String, possibles: [Character]) {
+        for char in pattern {
+            let matches = possibleMatches[char]!
+            if matches.count == 1 {
+                return
+            } else {
+                var intersection = [Character]()
+                for match in matches where possibles.contains(match) {
+                    intersection.append(match)
+                }
+                possibleMatches[char] = intersection
+            }
+        }
+    }
+
+    func is1(_ segments: String) -> Bool {
         segments.count == 2
     }
 
-    static func is4(_ segments: String) -> Bool {
+    func is4(_ segments: String) -> Bool {
         segments.count == 4
     }
 
-    static func is7(_ segments: String) -> Bool {
+    func is7(_ segments: String) -> Bool {
         segments.count == 3
     }
 
-    static func is8(_ segments: String) -> Bool {
+    func is8(_ segments: String) -> Bool {
         segments.count == 7
     }
 }
 
 struct Display {
-    let patterns: [String]
-    let outputDigits: [String]
+    let patterns: [Set<Character>]
+    let outputDigits: [Set<Character>]
 
     init(_ input: String) {
         let parts = input.split(separator: "|")
         assert(parts.count == 2)
-        patterns = parts.first!.split(separator: " ").map(String.init)
-        outputDigits = parts[1].split(separator: " ").map(String.init)
+        patterns = parts[0].split(separator: " ").map { pattern in
+            Set(Array(pattern))
+        }
+        outputDigits = parts[1].split(separator: " ").map { pattern in
+            Set(Array(pattern))
+        }
     }
 }
