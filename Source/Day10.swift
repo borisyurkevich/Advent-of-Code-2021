d3958cb41581a05fdd1ff3a6c746e1da4e30c4e3
Boris Yurkevich <boris@cocoaswitch.com>
2022-03-07 17:04:01 +0000

    Day 10 Part 2 done

--- Source/Day10.swift
+++ Source/Day10.swift
@@ -16,19 +16,27 @@ enum Day10 {
         var processor = LineProcess()
 
         for line in input.lines {
-            print("processing line \(line)")
             processor.process(line)
         }
-        print(processor.scores.points)
+        print("incomplete lines: \(processor.incompleteLines.count)")
+        for incopmpleteLine in processor.incompleteLines {
+            let reversed: [Character] = incopmpleteLine.reversed()
+            let chars = processor.findIncompleteCharacters(incompleteLine: reversed)
+            processor.scores.addAutocompleteScore(incompleCharacters: chars)
+        }
+        print(processor.scores.autompleteScore)
+        print("answer: \(processor.scores.findPart2Answer())")
     }
 }
 struct LineProcess {
 
     var chunks = [String]() // Each chunk must start and end with a character
     var scores = ScoreBoard()
+    var incompleteLines = [[Character]]()
 
     mutating func process(_ line: String) {
         var opens = [Character]()
+        var isCorupted = false
 
         for character in line {
             if isOpenining(char: character) {
@@ -39,13 +47,27 @@ struct LineProcess {
                 }
                 if closingCharacter(char: character, lastFromOpens: lastCharacter) {
                     // Remove
-                    _ = opens.removeLast()
+                    _ = opens.popLast()
                 } else {
-                    _ = opens.removeLast()
+                    _ = opens.popLast()
                     scores.addIllegalCharacter(character)
+                    isCorupted = true
                 }
             }
         }
+
+        // Collect incomplete lines only
+        if !opens.isEmpty && !isCorupted {
+            incompleteLines.append(opens)
+        }
+    }
+
+    func findIncompleteCharacters(incompleteLine: [Character]) -> [Character] {
+        var result = [Character]()
+        for char in incompleteLine {
+            result.append(closingCharacter(char: char))
+        }
+        return result
     }
 
     func isOpenining(char: Character) -> Bool {
@@ -71,11 +93,26 @@ struct LineProcess {
         }
         fatalError("unexpected input")
     }
+
+    func closingCharacter(char: Character) -> Character {
+        assert(isOpenining(char: char))
+        if char == "(" {
+            return ")"
+        } else if char == "{" {
+            return "}"
+        } else if char == "[" {
+            return "]"
+        } else if char == "<" {
+            return ">"
+        }
+        fatalError("unexpected input")
+    }
 }
 
 struct ScoreBoard {
 
     var points = 0
+    var autompleteScore = [Int]()
 
     mutating func addIllegalCharacter(_ char: Character) {
         guard let score = pointsTable[char] else {
@@ -84,10 +121,36 @@ struct ScoreBoard {
         points -= score
     }
 
+    mutating func addAutocompleteScore(incompleCharacters: [Character]) {
+        var result = 0
+        for char in incompleCharacters {
+            guard let score = autoCompleteTable[char] else {
+                fatalError()
+            }
+            result *= 5
+            result += score
+        }
+        autompleteScore.append(result)
+    }
+
     private let pointsTable: [Character : Int] = [
         ")" : 3,
         "]" : 57,
         "}" : 1197,
         ">" : 25137
     ]
+
+    // Part 2
+    private let autoCompleteTable: [Character : Int] = [
+        ")" : 1,
+        "]" : 2,
+        "}" : 3,
+        ">" : 4
+    ]
+
+    func findPart2Answer() -> Int {
+        let sorted = autompleteScore.sorted()
+        let index = (sorted.count - 1) / 2
+        return sorted[Int(index)]
+    }
 }
